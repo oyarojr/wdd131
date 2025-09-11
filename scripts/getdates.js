6eea17129a8662b3f13dda4eb617b3b67736d8ae
oyarojr <oyarodan2024@outlook.com>
2025-09-11 19:20:12 +0300

    Finally managed to complete week 1 work.

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year in the footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set the last modified date of the document
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
