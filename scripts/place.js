5778d21f7839dd327cab88d50b9c3c0046423b18
oyarojr <oyarodan2024@outlook.com>
2025-09-23 02:35:39 +0300

    formatted the webpage for desktop view and managed to create function for the windchill in javascript.

--- scripts/place.js
+++ scripts/place.js
@@ -1,6 +1,19 @@
-const temp = document.getElementById('#temp');
-const 
+const temp = document.querySelector('#temp').textContent;
+const wind = document.querySelector('#wind').textContent;
+const windchill = document.querySelector('#windchill');
 
+const tempint = parseInt(temp, 10);
+const windint = parseInt(wind, 10);
+
+
+function windch(temper, windy) {
+    return 13.12 + (0.6215 * temper) - 11.37 * (windy ** 0.16) + 0.3965 * temper * (windy ** 0.16);
+}
+
+const unfwindchi = windch(tempint, windint);
+const fowindchi = unfwindchi.toFixed(2);
+
+windchill.textContent = fowindchi;
 
 
 document.getElementById('currentyear').textContent = new Date().getFullYear();
--- week02/scripts/debugging.js
+++ week02/scripts/debugging.js
@@ -1,13 +1,13 @@
 const radiusOutput = document.getElementById('radius');
-const areaOutput = document.querySelector('area');
+const areaOutput = document.querySelector('#area');
 
 let area = 0;
 const PI = 3.14159;
 
 const radius = 10;
 area = PI * radius * radius;
-radiusOutput = radius;
-areaOutput = area;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
 
 radius = 20;
 area = PI * radius * radius;
