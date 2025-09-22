d421ea8f9d8dad77a8e1f67abe649b9a5065c67c
oyarojr <oyarodan2024@outlook.com>
2025-09-22 17:59:31 +0300

    Created the skeleton html,css & javascript for week 03 assignment. Also set grounds for mobile view

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+const temp = document.getElementById('#temp');
+const 
+
+
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
